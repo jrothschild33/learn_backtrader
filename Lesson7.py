4ef4be2d2ea819a3d0f957240f7044ef26ad1155
Jianan Zhou <dr_neutron@126.com>
2021-10-21 08:43:10 +0800

    2021-10-21

--- Lesson7.py
+++ Lesson7.py
@@ -287,9 +287,9 @@ class PlotScheme(object):
 
 # 通过参数形式来设置
 cerebro.plot(iplot=False,
-             style='candel', # 设置主图行情数据的样式为蜡烛图
+             style='candel',  # 设置主图行情数据的样式为蜡烛图
              lcolors=colors , # 重新设置主题颜色
-             plotdist=0.1, # 设置图形之间的间距
+             plotdist=0.1,    # 设置图形之间的间距
              barup = '#ff9896', bardown='#98df8a', # 设置蜡烛图上涨和下跌的颜色
              volup='#ff9896', voldown='#98df8a', # 设置成交量在行情上涨和下跌情况下的颜色
              ....)
@@ -474,6 +474,227 @@ cerebro1.plot(iplot=False,
               volup='#ff9896',
               voldown='#98df8a',
               loc='#5f5a41',
-              # # 蜡烛之间会比较拥挤，可以通过设置 numfigs=2，分 2 部分绘制
+              # 蜡烛之间会比较拥挤，可以通过设置 numfigs=2，分 2 部分绘制
               # numfigs=2,
               grid=False) # 删除水平网格
+
+# =============================================================================
+#%%
+# 第3章 基于收益序列进行可视化
+'''
+Backtrader 自带的绘图工具方便好用，不过平时在汇报策略回测结果时，可能更关注的是策略的累计收益曲线和业绩评价指标等结果，
+而这些回测统计信息只需基于回测返回的 TimeReturn 收益序列做简单计算即可得到。
+下面是基于 Backtrader 回测返回的分析器 TimeReturn、pyfolio、matplotlib 得到的可视化图形。
+'''
+import numpy as np
+import pandas as pd
+import backtrader as bt
+import warnings
+warnings.filterwarnings('ignore')
+
+import tushare as ts
+import json
+with open(r'Data/tushare_token.json','r') as load_json:
+    token_json = json.load(load_json)
+token = token_json['token']
+ts.set_token(token) 
+pro = ts.pro_api(token)
+
+# 使用Tushare获取数据，要严格保持OHLC的格式
+df = ts.pro_bar(ts_code='600276.SH', adj='qfq',start_date='20160101', end_date='20211015')
+df = df[['trade_date', 'open', 'high', 'low', 'close','vol']]
+df.columns = ['trade_date', 'open', 'high', 'low', 'close','volume']
+df.trade_date = pd.to_datetime(df.trade_date)
+# 索引必须是日期
+df.index = df.trade_date
+# 日期必须要升序
+df.sort_index(inplace=True)
+
+# Create a Stratey
+class TestStrategy(bt.Strategy):
+    # 设定参数，便于修改
+    params = (
+        ('maperiod', 60),
+    )
+
+    def log(self, txt, dt=None):
+        ''' 
+        日志函数：打印结果
+        datas[0]：传入的数据，包含日期、OHLC等数据
+        datas[0].datetime.date(0)：调用传入数据中的日期列
+        '''
+        dt = dt or self.datas[0].datetime.date(0)
+        # print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        # dataclose变量：跟踪当前收盘价
+        self.dataclose = self.datas[0].close
+
+        # order变量：跟踪订单状态
+        self.order = None
+        # buyprice变量：买入价格
+        self.buyprice = None
+        # buycomm变量：买入时佣金费用
+        self.buycomm = None
+
+        # 指标：简单移动平均 MovingAverageSimple【15天】
+        self.sma = bt.indicators.SimpleMovingAverage(self.datas[0],period=self.params.maperiod)
+        
+        # 添加画图专用指标
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25)
+
+        self.my_indicator_kdj = bt.indicators.StochasticFull(self.datas[0],period=9)
+        
+    def notify_order(self, order):
+        '''订单状态通知(order.status)：提示成交状态'''
+        if order.status in [order.Submitted, order.Accepted]:
+            # 如果订单只是提交状态，那么啥也不提示
+            return
+
+        # 检查订单是否执行完毕
+        # 注意：如果剩余现金不足，则会被拒绝！
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                # 买入信号记录：买入价、买入费用、买入佣金费用
+                self.log(
+                        'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                        (order.executed.price,
+                         order.executed.value,
+                         order.executed.comm))
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            elif order.issell():
+                # 卖出信号记录：卖出价、卖出费用、卖出佣金费用
+                self.log(
+                        'SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm %.2f' %
+                        (order.executed.price,
+                         order.executed.value,
+                         order.executed.comm))
+            
+            # 记录订单执行的价格柱的编号（即长度）
+            self.bar_executed = len(self)
+
+        # 如果订单被取消/保证金不足/被拒绝
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        # 如果没有查询到订单，则订单为None
+        self.order = None
+
+    def notify_trade(self, trade):
+        '''交易状态通知：查看交易毛/净利润'''
+        if not trade.isclosed:
+            return
+        # 交易毛利润：trade.pnl、交易净利润：trade.pnlcomm（扣除佣金）
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                    (trade.pnl, trade.pnlcomm))
+
+    # 交易策略主函数
+    def next(self):
+        # 记录收盘价
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        # 检查一下是否有订单被挂起，如果有的话就先不下单
+        if self.order:
+            return
+        
+        # 检查一下目前是否持有头寸，如果没有就建仓
+        if not self.position:
+
+            # 如果 K角度>0 且 K>D 且  K<50:
+            if (self.my_indicator_kdj.percK[0] > self.my_indicator_kdj.percK[-1]) & (self.my_indicator_kdj.percK[0] > self.my_indicator_kdj.percD[0]) & (self.my_indicator_kdj.percK[0]<20):
+                # 买买买！先记录一下买入价格（收盘价）
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                # 更新订单状态：buy()：开仓买入，买入价是下一个数据，即【开盘价】
+                self.order = self.buy()
+        else:
+             # 如果已经建仓，并持有头寸，则执行卖出指令
+             # 如果 K角度<0 且 K<D 且 K>70:
+            if (self.my_indicator_kdj.percK[0] < self.my_indicator_kdj.percK[-1]) & (self.my_indicator_kdj.percK[0] < self.my_indicator_kdj.percD[0]):
+                # 卖!卖!卖!
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                # 更新订单状态：sell()：平仓卖出，卖出价是下一个数据，即【开盘价】
+                self.order = self.sell()
+
+# 创建实例
+cerebro = bt.Cerebro()
+ # 添加策略
+cerebro.addstrategy(TestStrategy)
+# 添加数据源
+data = bt.feeds.PandasData(dataname=df)
+# 输入数据源
+cerebro.adddata(data)
+# 设置初始现金：10万
+cerebro.broker.setcash(1000000.0)
+# 设定每次买入的股票数量：10股
+cerebro.addsizer(bt.sizers.FixedSize, stake=1000)
+# 设置佣金费率：双边0.1%
+cerebro.broker.setcommission(commission=0.001)
+
+
+# 回测时需要添加 TimeReturn 分析器
+cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='_TimeReturn')
+result = cerebro.run()
+
+# 提取收益序列
+pnl = pd.Series(result[0].analyzers._TimeReturn.get_analysis())
+# 计算累计收益
+cumulative = (pnl + 1).cumprod()
+# 计算回撤序列
+max_return = cumulative.cummax()
+drawdown = (cumulative - max_return) / max_return
+
+# 计算收益评价指标
+import pyfolio as pf
+# 按年统计收益指标
+perf_stats_year = (pnl).groupby(pnl.index.to_period('y')).apply(lambda data: pf.timeseries.perf_stats(data)).unstack()
+# 统计所有时间段的收益指标
+perf_stats_all = pf.timeseries.perf_stats((pnl)).to_frame(name='all')
+perf_stats = pd.concat([perf_stats_year, perf_stats_all.T], axis=0)
+perf_stats_ = round(perf_stats,4).reset_index()
+
+
+# 绘制图形
+import matplotlib.pyplot as plt
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+import matplotlib.ticker as ticker # 导入设置坐标轴的模块
+# plt.style.use('seaborn') 
+plt.style.use('dark_background')
+
+fig, (ax0, ax1) = plt.subplots(2,1, gridspec_kw = {'height_ratios':[1.5, 4]}, figsize=(20,8))
+cols_names = ['date', 'Annual\nreturn', 'Cumulative\nreturns', 'Annual\nvolatility',
+             'Sharpe\nratio', 'Calmar\nratio', 'Stability', 'Max\ndrawdown',
+             'Omega\nratio', 'Sortino\nratio', 'Skew', 'Kurtosis', 'Tail\nratio',
+             'Daily value\nat risk']
+
+# 绘制表格
+ax0.set_axis_off() # 除去坐标轴
+table = ax0.table(cellText = perf_stats_.values,
+                bbox=(0,0,1,1), # 设置表格位置， (x0, y0, width, height)
+                rowLoc = 'right', # 行标题居中
+                cellLoc='right' ,
+                colLabels = cols_names, # 设置列标题
+                colLoc = 'right', # 列标题居中
+                edges = 'open' # 不显示表格边框
+                )
+table.set_fontsize(13)
+
+# 绘制累计收益曲线
+ax2 = ax1.twinx()
+ax1.yaxis.set_ticks_position('right') # 将回撤曲线的 y 轴移至右侧
+ax2.yaxis.set_ticks_position('left') # 将累计收益曲线的 y 轴移至左侧
+# 绘制回撤曲线
+drawdown.plot.area(ax=ax1, label='drawdown (right)', rot=0, alpha=0.3, fontsize=13, grid=False)
+# 绘制累计收益曲线
+(cumulative).plot(ax=ax2, color='#F1C40F' , lw=3.0, label='cumret (left)', rot=0, fontsize=13, grid=False)
+# 不然 x 轴留有空白
+ax2.set_xbound(lower=cumulative.index.min(), upper=cumulative.index.max())
+# 主轴定位器：每 5 个月显示一个日期：根据具体天数来做排版
+ax2.xaxis.set_major_locator(ticker.MultipleLocator(100))
+# 同时绘制双轴的图例
+h1,l1 = ax1.get_legend_handles_labels()
+h2,l2 = ax2.get_legend_handles_labels()
+plt.legend(h1+h2,l1+l2, fontsize=12, loc='upper left', ncol=1)
+
+fig.tight_layout() # 规整排版
+plt.show()
